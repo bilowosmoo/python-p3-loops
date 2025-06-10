600658ea6f7f7387e34a137588542c2e97444a23
bilow osmoo <bilowosmo@gmail.com>
2025-06-10 22:19:12 +0300

    done

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
-#!/usr/bin/env python3
-
+# 1. Happy New Year Countdown using while loop
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
 
-def square_integers(int_list):
-    # code goes here!
-    pass
 
+# 2. Square integers in a list
+def square_integers(numbers):
+    return [num ** 2 for num in numbers]
+
+
+# 3. FizzBuzz from 1 to 100
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
